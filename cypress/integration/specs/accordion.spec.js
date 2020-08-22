741f210fe236d9b4eaed10e9f3f0ee0a5a50579f
Anowar Hossain <anowar.hossain@bounteous.com>
2020-08-22 04:58:27 -0400

    Adding new tests for Load more button & Accordion

---
+++ cypress/integration/specs/accordion.spec.js
@@ -0,0 +1,33 @@
+
+/// <reference types="cypress" />
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
+  
+  context('Verify Accordions', () => {
+    
+    beforeEach(() => {
+      cy.HomePage()
+      cy.AcceptCookie()
+    })
+
+    it('Verify Accordion on Career Page', () =>{
+      cy.contains('Join Our Team').click()
+      cy.url().should('include', '/careers')
+      cy.title().should('eq','Healthcare Technology Jobs | Careers | Change Healthcare')
+      cy.contains('Total Rewards').should('be.visible')
+      cy.contains('Empowering Your Health').should('have.attr', 'class','cmp-tabs__title cmp-tabs--active')
+      cy.contains('Supporting Your Community').click().should('have.attr', 'class','cmp-tabs__title cmp-tabs--active').click()
+      cy.contains('Two volunteer days off').should('be.visible')
+
+    })
+
+    it.only('Verify accordion on Payment accuracy page', () =>{
+      
+       cy.contains('Solutions').click()
+       //cy.url().should('contains','enrollment-services')
+    })
+     
+    })
